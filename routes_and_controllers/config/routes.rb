3c7dc8a45077c3eafb4ed2b4ca614c7c7d197b1e
chakhoic <chakhoichan@gmail.com>
2022-12-14 17:14:17 -0500

    chakwilson

--- routes_and_controllers/config/routes.rb
+++ routes_and_controllers/config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get 'users/new', to: 'users#new', as: 'new_user'
   get '/users', to: 'users#index', as: 'users'
   post 'users', to: 'users#create'
-  get '/users/:id', to: 'users#edit', as: 'edit_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   get '/users/:id', to: 'users#show', as: 'user'
   patch '/users/:id', to: 'users#update'
   put '/users/:id', to: 'users#update'
